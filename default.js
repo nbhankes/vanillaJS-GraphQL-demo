88098c2336add60c9659ceba5f2cf92d33fc5ebf
nbankes <nbhankes@gmail.com>
2021-05-02 14:45:29 -0700

    Initial Commit

---
+++ default.js
@@ -0,0 +1,59 @@
+//Modified from a tutorial by Nolan Kovacik at https://medium.com/@noltron000/graphql-vanillajs-in-minutes-e5457e27e85b
+
+/* STEP 1: USE A MODULE */
+// Using HTML modules, we'll import this project:
+// https://github.com/octokit/graphql.js/
+import {graphql} from 'https://cdn.pika.dev/@octokit/graphql'
+
+
+/* STEP 2: PREPARE FOR REQUEST */
+// We'll need three things for our request:
+// - the GraphQL query that we want called
+// - our GitHub Access Token
+// - the Authorization Header, with our token
+
+// If you'd like to mess around with the GitHub API,
+// visit the GraphiQL playground here:
+// https://developer.github.com/v4/explorer/
+const query = `
+# GraphQL query comments use a hashtag.
+# Create queries at the GitHub API Explorer: https://docs.github.com/en/graphql/overview/explorer
+query {
+  user(login: "nbhankes") {
+    repositories {
+      totalCount
+    }
+    bio
+    avatarUrl
+    contributionsCollection {
+      contributionYears
+      totalCommitContributions
+    }
+  }
+}
+`
+
+// Create your github access token at: https://github.com/settings/tokens
+// GitHub recommends allowing specific scoped permissions. See list at: https://docs.github.com/en/graphql/guides/forming-calls-with-graphql
+const token = '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'
+const auth = {
+    headers: {
+        authorization: 'token ' + token
+    }
+}
+
+
+/* STEP 3: CREATE THE FUNCTION */
+// Create an async function to make the request.
+async function niceRequest(query, auth) {
+   return await graphql(query, auth)
+}
+// You can use ES6 syntax to make it semantic and in one-line!
+    //const niceRequest = async (q, a) => await graphql(q, a)
+
+
+/* STEP 4: USE THE FUNCTION */
+// This will resolve the promise and print it to console.
+// You can expand the objects and subobjects to see data.
+console.log(niceRequest(query, auth))
+// Congratulations! You ran a VanillaJS GraphQL query!
