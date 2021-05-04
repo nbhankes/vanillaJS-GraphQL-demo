80e21587d0bd7ce3721b45ae31bc06b02bdcbeb6
nbankes <nbhankes@gmail.com>
2021-05-03 19:55:12 -0700

    Updated html, js, css files

--- default.js
+++ default.js
@@ -1,55 +1,38 @@
-//Modified from a tutorial by Nolan Kovacik at https://medium.com/@noltron000/graphql-vanillajs-in-minutes-e5457e27e85b
+// Querying the GraphQL API using the Fetch() API
 
-/* STEP 1: USE A MODULE */
-// Using HTML modules, we'll import this project:
-// https://github.com/octokit/graphql.js/
-import {graphql} from 'https://cdn.pika.dev/@octokit/graphql'
-
-
-/* STEP 2: PREPARE FOR REQUEST */
-// We'll need three things for our request:
-// - the GraphQL query that we want called
-// - our GitHub Access Token
-// - the Authorization Header, with our token
-
-// If you'd like to mess around with the GitHub API,
-// visit the GraphiQL playground here:
-// https://developer.github.com/v4/explorer/
-const query = `
-# GraphQL query comments use a hashtag.
-# Create queries at the GitHub API Explorer: https://docs.github.com/en/graphql/overview/explorer
-query {
-  user(login: "ADD_YOUR_USERNAME_HERE") {
-    repositories {
-      totalCount
-    }
-    bio
-    avatarUrl
-    contributionsCollection {
-      contributionYears
-      totalCommitContributions
-    }
-  }
-}
-`
-
-// Create your github access token at: https://github.com/settings/tokens
-// GitHub recommends allowing specific scoped permissions. See list at: https://docs.github.com/en/graphql/guides/forming-calls-with-graphql
-const token = 'ADD_YOUR_TOKEN_HERE'
-const auth = {
+fetch('https://api.spacex.land/graphql/', {
+    method: "POST",
     headers: {
-        authorization: 'token ' + token
-    }
-}
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+        query: `
+            query {
+                company {
+                  ceo
+                  name
+                  founded
+                  summary
+                }
+              }
+              
+          `,
+}),
+})
+.then((res) => res.json())
+.then((result) => {
+    document.querySelector(".query-response").innerHTML = `
+    <article class="ceo-card-container">
+        <h1>${result.data.company.name}</h1>
+        <h3>CEO: ${result.data.company.ceo}</h3>
+        <h3>Founded: ${result.data.company.founded}</h3>
+        <h4>${result.data.company.summary}</h4>
+    </article>
+    `
+})
 
 
-/* STEP 3: CREATE THE FUNCTION */
-// Create an async function to make the request.
-async function niceRequest(query, auth) {
-   return await graphql(query, auth)
-}
 
-/* STEP 4: USE THE FUNCTION */
-// This will resolve the promise and print it to console.
-// You can expand the objects and subobjects to see data.
-console.log(niceRequest(query, auth))
+
+
+
