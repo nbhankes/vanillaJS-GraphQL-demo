cda650de3a93b2307befc75a80affee35d0f2efb
nbankes <nbhankes@gmail.com>
2021-05-03 20:05:32 -0700

    README updated

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 ## Welcome.
 
-This tutorial is an adaptation on Nolan Kovacik's tutorial [GraphQL + VanillaJS in Minutes](https://medium.com/@noltron000/graphql-vanillajs-in-minutes-e5457e27e85b)
-
 To get this demo working on your local environment, follow these steps:
 
 1. Clone the repo to your local system using the Git CLI language:
    git clone https://github.com/nbhankes/vanillaJS-GraphQL-demo.git
 2. Open the vanillaJS-GraphQL-demo folder in your editor of choice
-3. Follow the directions in default.js file Step 2 to get your GitHub access token. Add your unique access token where indicated. And within the query, add your GitHub username where indicated.
-4. Now run the program using an extension like [Live Server](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer)
-5. Now use the browser dev tools to access the console. You will see your query displayed there.
+3. Now run the program using an extension like [Live Server](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer)
+4. Study the code comments to understand what's happening.
+5. Break it. Fix it. Learn from it. Customize it to your needs.
--- default.js
+++ default.js
@@ -1,10 +1,15 @@
 // Querying the GraphQL API using the Fetch() API
 
 fetch('https://api.spacex.land/graphql/', {
+// We're technically receiving data, so GET might be more appropiate, however 
+// POST seems to work always. 
     method: "POST",
+// The header "Content-Type" tells the software that it is receiving JSON data.
     headers: {
         'Content-Type': 'application/json',
     },
+// Queries and variables need to be sent as JSON Object
+// This query was generated in the GraphiQL explorer at https://api.spacex.land/graphql/
     body: JSON.stringify({
         query: `
             query {
@@ -17,9 +22,14 @@ fetch('https://api.spacex.land/graphql/', {
               }
               
           `,
+// Even if you don't have variables, you will avoid issues by 
+// sending an empty variable object.
+  variables: {},
 }),
 })
+// A promise chain: Once the above query is sent, then the below code turns the response to JSON
 .then((res) => res.json())
+// A promise chain: Once the above code is executed, the the below code renders it to the browser.
 .then((result) => {
     document.querySelector(".query-response").innerHTML = `
     <article class="ceo-card-container">
